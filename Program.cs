4554fef4f64f5666fb7a41271f750c63834ad89e
Marcel N <marcel.nita@gmail.com>
2021-11-14 23:01:18 +0200

    Workaround for when running without a terminal

--- Program.cs
+++ Program.cs
@@ -11,9 +11,16 @@ namespace docker_file_sample
             var i = 0;
             while (true)
             {
+                if(!Console.IsInputRedirected && Console.KeyAvailable)
+                {
+                    break;
+                }
+
                 Console.WriteLine($"Counter: {++i}");
                 Thread.Sleep(1000);
             }
+
+            Console.WriteLine("Stopping");
         }
     }
 }
