e71afc3dd929d2309e1dfa98bc2f9d2b5890d41c
Marcel N <marcel.nita@gmail.com>
2021-11-14 21:14:24 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace docker_file_sample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var i = 0;
+            while (!Console.KeyAvailable)
+            {
+                Console.WriteLine($"Counter: {++i}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Stopping");
+        }
+    }
+}
